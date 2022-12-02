99460f832db235ee7ceb09d81049020a2651cc17
DeckardZhao <deckardz46@outlook.com>
2022-12-02 17:21:25 +0800

    Event system done

--- Novl/src/Novl/Event/Event.h
+++ Novl/src/Novl/Event/Event.h
@@ -44,4 +44,25 @@ namespace Novl {
 	protected:
 		bool m_Handled = false;
 	};
+
+	class EventDispatcher {
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			:m_Event(event) {
+
+		}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func) {
+			if (m_Event.GetEventType() == T::GetStaticType()) {
+				m_Event.m_Handled = func(*(T*)&m_Event); // black magic???
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
 }
--- Novl/src/Novl/Event/KeyEvent.h
+++ Novl/src/Novl/Event/KeyEvent.h
@@ -3,8 +3,8 @@
 #include <sstream>
 
 namespace Novl{
-	class NOVL_API KeyEvent :
-		public Event {
+	class NOVL_API KeyEvent 
+		: public Event {
 	public:
 		inline int GetKeyCode() const { return m_KeyCode; }
 
@@ -14,5 +14,41 @@ namespace Novl{
 		KeyEvent(int keycode):m_KeyCode(keycode){}
 
 		int m_KeyCode;
+	};
+
+	class NOVL_API KeyPressedEvent :
+		public KeyEvent {
+	public:
+		KeyPressedEvent(int keycode, int repeatCount) :
+			KeyEvent(keycode), m_RepeatCount(repeatCount) {}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed)
+
+	private:
+		int m_RepeatCount;
+	};
+
+	class NOVL_API KeyReleasedEvent 
+		: public KeyEvent {
+	public:
+		KeyReleasedEvent(int keycode) 
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode ;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+		
 	};
 }
